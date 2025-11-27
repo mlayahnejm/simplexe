303d01e1ba88c701aca49884b423142f0344d689
mlayahnejm <mlayahnejm@yahoo.fr>
2025-11-27 15:18:16 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,1 +1,16 @@
 
+<?php
+
+/**
+ * Configuration for database connection
+ *
+ */
+
+$host       = "simpleweb-bdd";
+$username   = "root";
+$password   = "azerty";
+$dbname     = "test";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
